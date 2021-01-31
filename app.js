15ea3000914c1c4d044a953bc7fec79a4dd7c084
shwetanshutech <shwetanshu2000@gmail.com>
2021-01-31 14:04:03 +0530

    first deploy

---
+++ app.js
@@ -0,0 +1,52 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+const GoogleSpreadsheet = require("google-spreadsheet");
+const { promisify } = require("util");
+
+const creds = require("./client_secret.json");
+
+let finalarr = [];
+const port = process.env.PORT || 9000;
+
+function addtoarr(row) {
+  let arr = [];
+  arr.push(row.coursecode);
+  arr.push(row.coursename);
+  arr.push(row.facultyname);
+  arr.push(row.slot);
+  arr.push(row.grouplink);
+  finalarr.push(arr);
+}
+
+async function accessSpreadsheet() {
+  const doc = new GoogleSpreadsheet(
+    "1Gg2PGrtcGEG2uQtXeioAqrpcNbYtqywNudVssEB2SNE"
+  );
+  await promisify(doc.useServiceAccountAuth)(creds);
+  const info = await promisify(doc.getInfo)();
+  const sheet = info.worksheets[0];
+  // console.log(sheet.title);
+  const rows = await promisify(sheet.getRows)({
+    offset: 1,
+  });
+  rows.forEach((row) => {
+    addtoarr(row);
+  });
+}
+accessSpreadsheet();
+
+app.get("/", function (req, res) {
+  let values = finalarr;
+  res.render("try", { values: values });
+});
+app.get("*", function (req, res) {
+  res.send("Sorry ! You are lost!");
+});
+
+app.listen(port, function () {
+  console.log("App listening!");
+});
---
+++ spreadsheet.js
@@ -0,0 +1,42 @@
+const GoogleSpreadsheet = require('google-spreadsheet');
+const { promisify } = require('util');
+
+const creds = require('./client_secret.json');
+
+let finalarr = [];
+
+function addtoarr(row) {
+    let arr = [];
+    arr.push(row.coursename);
+    // console.log(`${row.coursename}`);
+    arr.push(row.slot);
+    finalarr.push(arr);
+}
+
+async function accessSpreadsheet() {
+    const doc = new GoogleSpreadsheet('1Gg2PGrtcGEG2uQtXeioAqrpcNbYtqywNudVssEB2SNE');
+    await promisify(doc.useServiceAccountAuth)(creds);
+    const info = await promisify(doc.getInfo)();
+    const sheet = info.worksheets[0];
+    // console.log(sheet.title);
+    const rows = await promisify(sheet.getRows)({
+        offset: 1
+    });
+    rows.forEach(row => {
+        addtoarr(row);
+    });
+    console.log(finalarr);
+}
+accessSpreadsheet();
+// https://docs.google.com/spreadsheets/d/1Gg2PGrtcGEG2uQtXeioAqrpcNbYtqywNudVssEB2SNE/edit#gid=0
+
+// async function accessSpreadsheet() {
+//     await doc.useServiceAccountAuth({
+//         client_email: creds.client_email,
+//         private_key: creds.private_key,
+//     });
+
+//     await doc.loadInfo(); // loads document properties and worksheets
+//     console.log(doc.title);
+
+// }
